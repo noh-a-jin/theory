05761ae68ea80473120ff400ddd838bc303f38a0
노아진 <you@example.com>
2025-08-05 20:41:24 +0900

    띠로리

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Hello from "./hello"
+
+function App() {
+  
+
+  return (
+    <>
+     <Hello></Hello>
+      <Hello></Hello>
+       <Hello></Hello>
+        <Hello></Hello>
+         <Hello></Hello>
+    </>
+  )
+}
+
+export default App
